e9124d6be9d8030a9b04eeb227f6678ef635a806
Veronica <161347755+hinA62@users.noreply.github.com>
2026-03-10 17:24:13 +0200

    analysys

--- SmartGreenhouseControlSystem.Application/obj/Debug/net10.0/SmartGreenhouseControlSystem.Application.AssemblyInfo.cs
+++ SmartGreenhouseControlSystem.Application/obj/Debug/net10.0/SmartGreenhouseControlSystem.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartGreenhouseControlSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d001c7cd134c78a333a55621c8a8f07294b72286")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af5a3bdcc8bd85aa63feba9c5718efdb257fd47")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartGreenhouseControlSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartGreenhouseControlSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
